ccd3a94bea1a08b4159e86f181a93a8d1ff21a37
luteh <luthfanmaftuh@gmail.com>
2019-08-31 06:01:14 +0700

    Implement getallcontacts api to view

--- app/src/main/java/com/luteh/contactapps/ui/listcontacts/ListContactsActivity.kt
+++ app/src/main/java/com/luteh/contactapps/ui/listcontacts/ListContactsActivity.kt
@@ -1,13 +1,80 @@
 package com.luteh.contactapps.ui.listcontacts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
+import androidx.recyclerview.widget.RecyclerView
 import com.luteh.contactapps.R
+import com.luteh.contactapps.ui.MyViewModelFactory
+import com.luteh.contactapps.ui.listcontacts.adapter.ListContactsAdapter
+import kotlinx.android.synthetic.main.list_contacts_activity.*
+import org.jetbrains.anko.longToast
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
 
-class ListContactsActivity : AppCompatActivity() {
+class ListContactsActivity : AppCompatActivity(), KodeinAware, ListContactsNavigator {
+
+    override val kodein by closestKodein()
+    private val viewModelFactory: MyViewModelFactory by instance()
+
+    private lateinit var viewModel: ListContactsViewModel
+
+    private val listContactsAdapter = ListContactsAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_contacts_activity)
+
+        onInit()
+    }
+
+    private fun onInit() {
+        initViewModel()
+        observeData()
+        setupRecyclerView()
+
+        viewModel.getAllContacts()
+    }
+
+    private fun initViewModel() {
+        viewModel =
+            ViewModelProviders.of(this, viewModelFactory).get(ListContactsViewModel::class.java)
+        viewModel.mNavigator = this
+    }
+
+    private fun observeData() {
+        viewModel.mIsLoading.observe(this, Observer {
+            pb_list_contacts.visibility = if (it) View.VISIBLE else View.GONE
+            rv_list_contacts.visibility = if (it) View.GONE else View.VISIBLE
+        })
+
+        viewModel.allContactsData.observe(this, Observer {
+            listContactsAdapter.setDataSources(it)
+        })
+    }
+
+    private fun setupRecyclerView() {
+        rv_list_contacts.apply {
+            setHasFixedSize(true)
+            addItemDecoration(DividerItemDecoration(context, VERTICAL))
+            adapter = listContactsAdapter
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    if (dy > 0 && fab_list_contacts_add.isShown)
+                        fab_list_contacts_add.hide()
+                    else
+                        fab_list_contacts_add.show()
+                }
+            })
+        }
+    }
+
+    override fun onSuccessSaveContact(message: String) {
+        longToast(message)
     }
 }
--- app/src/main/java/com/luteh/contactapps/ui/listcontacts/adapter/ListContactsHolder.kt
+++ app/src/main/java/com/luteh/contactapps/ui/listcontacts/adapter/ListContactsHolder.kt
@@ -2,7 +2,9 @@ package com.luteh.contactapps.ui.listcontacts.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.luteh.contactapps.R
 import com.luteh.contactapps.data.model.getallcontacts.GetAllContactsData
+import com.luteh.contactapps.internal.GlideApp
 import kotlinx.android.synthetic.main.list_contacts_item.view.*
 
 /**
@@ -15,6 +17,10 @@ class ListContactsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         getAllContactsData.let {
             tv_list_contacts_name_item.text = "${it.firstName} ${it.lastName}"
             tv_list_contacts_age_item.text = "${it.age} years old"
+
+            GlideApp.with(this)
+                .load(R.drawable.ic_account_circle)
+                .into(iv_list_contacts_item)
         }
     }
 }
