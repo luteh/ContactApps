13a9679fa22408a760c5c157b3a27853276a51fe
luteh <luthfanmaftuh@gmail.com>
2019-08-31 06:23:10 +0700

    Implement bottom sheet on view

--- app/src/main/java/com/luteh/contactapps/ui/listcontacts/ListContactsActivity.kt
+++ app/src/main/java/com/luteh/contactapps/ui/listcontacts/ListContactsActivity.kt
@@ -1,17 +1,22 @@
 package com.luteh.contactapps.ui.listcontacts
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.luteh.contactapps.R
 import com.luteh.contactapps.ui.MyViewModelFactory
 import com.luteh.contactapps.ui.listcontacts.adapter.ListContactsAdapter
 import kotlinx.android.synthetic.main.list_contacts_activity.*
+import kotlinx.android.synthetic.main.list_contacts_sheet.view.*
 import org.jetbrains.anko.longToast
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.closestKodein
@@ -26,6 +31,9 @@ class ListContactsActivity : AppCompatActivity(), KodeinAware, ListContactsNavig
 
     private val listContactsAdapter = ListContactsAdapter()
 
+    private lateinit var mBottomSheetBehavior: BottomSheetBehavior<View>
+    private var mBottomSheetDialog: BottomSheetDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_contacts_activity)
@@ -34,11 +42,16 @@ class ListContactsActivity : AppCompatActivity(), KodeinAware, ListContactsNavig
     }
 
     private fun onInit() {
+        mBottomSheetBehavior = BottomSheetBehavior.from(layout_list_contacts_bottom_sheet)
+
         initViewModel()
         observeData()
         setupRecyclerView()
 
         viewModel.getAllContacts()
+        fab_list_contacts_add.setOnClickListener {
+            showBottomSheetDialog()
+        }
     }
 
     private fun initViewModel() {
@@ -71,6 +84,29 @@ class ListContactsActivity : AppCompatActivity(), KodeinAware, ListContactsNavig
                         fab_list_contacts_add.show()
                 }
             })
+        }
+    }
+
+    private fun showBottomSheetDialog() {
+        if (mBottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED)
+            mBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        val view = LayoutInflater.from(this).inflate(R.layout.list_contacts_sheet, null)
+
+        with(view) {
+            btn_list_contacts_close_sheet.setOnClickListener {
+                mBottomSheetDialog?.hide()
+            }
+        }
+
+        mBottomSheetDialog = BottomSheetDialog(this)
+        mBottomSheetDialog?.apply {
+            setContentView(view)
+            window?.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            setOnDismissListener {
+                mBottomSheetDialog = null
+            }
+            show()
         }
     }
 
