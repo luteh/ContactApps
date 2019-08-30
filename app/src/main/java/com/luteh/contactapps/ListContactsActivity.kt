a2def5c1c14a91fce3c2d077fecddc5110159bb4
luteh <luthfanmaftuh@gmail.com>
2019-08-30 10:36:40 +0700

    Initial commit

---
+++ app/src/main/java/com/luteh/contactapps/ListContactsActivity.kt
@@ -0,0 +1,12 @@
+package com.luteh.contactapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListContactsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_contacts_activity)
+    }
+}
